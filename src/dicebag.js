8a98fc43fa8194581292c77bca11d297942aaf80
Aniello12 <aniello.petruccelli04@gmail.com>
2022-02-11 00:11:43 +0100

    Dice bag commented

--- src/dicebag.js
+++ src/dicebag.js
@@ -1,13 +1,31 @@
-import Dice from "./dice.js";
+import Dice from "./dice.js"; //Dice class
+
 export default class DiceBag {
+  /**
+   *  This object is the Dice Container. Create an array of Dice elemets.
+   *
+   */
   constructor() {
     this.dicebag = [];
   }
 
+  /**
+   *  Create a Dice and add it to the dice Array
+   * 
+   * @param {+}  Dice Configuration
+   *
+   */
   createNewDice(config) {
     let dice = new Dice(config);
-    this.dicebag.push(dice);
+    this.dicebag.push(dice); //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/push
   }
+
+  /**
+   * The draw function renders a dice in a specified <div id="elemnt"></div>
+   *
+   * @param {element} id of parent node
+   * 
+   */
 
   draw(element) {
     let bag = document.getElementById(element);

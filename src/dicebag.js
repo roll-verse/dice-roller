5e1557c7cbe5744423ac581894fd51716ead739c
svilsoftwarescf <svilupposoftware@scfgroup.it>
2022-02-10 22:38:44 +0100

    DiceBagger

--- src/dicebag.js
+++ src/dicebag.js
@@ -1,10 +1,19 @@
+import Dice from "./dice.js";
 export default class DiceBag {
+  constructor() {
+    this.dicebag = [];
+  }
 
-    constructor(){
-        this.dicebag = {};
-    }
+  createNewDice(config) {
+    let dice = new Dice(config);
+    this.dicebag.push(dice);
+  }
 
-    createNewDice(){
-        
-    }
-} 
+  draw(element) {
+    let bag = document.getElementById(element);
+    bag.textContent = "";
+    this.dicebag.forEach((dice) => {
+      dice.renderDice(element);
+    });
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,29 +1,12 @@
-//import "./styles.css";
-//import "./dices.css";
-import Dice from "./dice.js";
 import DiceBag from "./dicebag.js";
 
+let MyBag = new DiceBag();
 
-let D4 = new Dice({ faces: 4 , base_modifier:-4});
-let D4Element = D4.renderDice("app");
-//D4.rollTheDice();
+MyBag.createNewDice({ faces: 4, base_modifier: -4 });
+MyBag.createNewDice({ faces: 8 });
+MyBag.draw("app");
 
-let D8 = new Dice({ faces: 8 });
-let D8Element = D8.renderDice("app");
-//D8.rollTheDice();
-
-let D10 = new Dice({ faces: 10 });
-let D10Element = D10.renderDice("app");
-//D10.rollTheDice();
-
-let D12 = new Dice({ faces: 12, dice_name:"Spadone", base_modifier:1  });
-let D12Element = D12.renderDice("app");
-//D12.rollTheDice();
-
-let D20 = new Dice({ faces: 20 });
-let D20Element = D20.renderDice("app");
-//D20.rollTheDice();
-
-let D100 = new Dice({ faces: 100 });
-let D100Element = D100.renderDice("app");
-//D100.rollTheDice();
+let MyBag2 = new DiceBag();
+MyBag2.createNewDice({ faces: 6, base_modifier: -4 });
+MyBag2.createNewDice({ faces: 18 });
+MyBag2.draw("app2");

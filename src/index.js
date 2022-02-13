2ea0273b1c3e20c756080c578a5cfc3dad08479a
AlfonsoPellegrino <98321430+AlfonsoPellegrino@users.noreply.github.com>
2022-02-13 17:57:50 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,9 +4,4 @@ let MyBag = new DiceBag();
 
 MyBag.createNewDice({ faces: 4, base_modifier: -4 });
 MyBag.createNewDice({ faces: 8 });
-MyBag.draw("app");
-
-let MyBag2 = new DiceBag();
-MyBag2.createNewDice({ faces: 6, base_modifier: -4 });
-MyBag2.createNewDice({ faces: 18 });
-MyBag2.draw("app2");
+MyBag.draw();
